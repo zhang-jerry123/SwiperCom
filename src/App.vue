640c77f0f5c7719e662f168b747d1b14869e6312
xixibao <xixibao@xixibaodeMBP.home>
2021-04-22 20:20:38 +0200

    formatted code

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,74 @@
 <template>
   <div>
-    <h3>轮播一些文字</h3>
-    <Swiper class="swiper_text" :content='texts' :duration='5' @onSwiperChange="changeActive">
-      <span>{{texts[active]}}</span>
+    <h3>Swiper some text</h3>
+    <Swiper
+      class="swiper_text"
+      :content="texts"
+      :duration="5"
+      @onSwiperChange="changeActive"
+    >
+      <span>{{ texts[active] }}</span>
     </Swiper>
-    <h3>纵向轮播</h3>
-    <Swiper class="swiper_img" :content="images" :direction=direct[i]>
+    <h3>Vertical Swiper</h3>
+    <Swiper class="swiper_img" :content="images" :direction="direct[i]">
     </Swiper>
-    <br>
+    <br />
     <button @click="clickchange">Change direction</button>
   </div>
 </template>
 
 <script>
-import Swiper from './components/swiper.vue'
+import Swiper from "./components/swiper.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Swiper
   },
-  data(){
-    return{
-      active:0,
-      direct:["column-reverse","column"],
-      i:0,
-      texts:[
-        'vue-swiper',
-        'From Sissi_Li',
-        '炸鸡&&薯条&&啤酒'
-      ],
-      images:[
-        'https://dummyimage.com/360x240/fff3cd/333&text=Swiper_xixi',
-        'https://dummyimage.com/360x240/d6d8d9/333&text=Swiper_xixi',
-        'https://dummyimage.com/360x240/cce5ff/333&text=Swiper_xixi'
+  data() {
+    return {
+      active: 0,
+      direct: ["column-reverse", "column"],
+      i: 0,
+      texts: ["vue-swiper", "From Sissi_Li", "2021-4-22"],
+      images: [
+        "https://dummyimage.com/360x240/fff3cd/333&text=Swiper_xixi",
+        "https://dummyimage.com/360x240/d6d8d9/333&text=Swiper_xixi",
+        "https://dummyimage.com/360x240/cce5ff/333&text=Swiper_xixi"
       ]
-    }
+    };
   },
-  methods:{
-    changeActive(n){
-      this.active=n
+  methods: {
+    changeActive(n) {
+      this.active = n;
     },
-    clickchange(){
-      if(this.i==0){
-        this.i++
-      }else{
-        this.i = 0
+    clickchange() {
+      if (this.i === 0) {
+        this.i++;
+      } else {
+        this.i = 0;
       }
       // console.log(this.direct[this.i])
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-.swiper{
-  &_text{
+.swiper {
+  &_text {
     width: 150px;
     text-align: center;
-    margin:0 auto;
-
+    margin: 0 auto;
   }
-  &_img{
+  &_img {
     height: 240px;
     text-align: center;
-    margin:0 auto;
+    margin: 0 auto;
   }
 }
 
-button{
+button {
   background-color: rgb(199, 206, 206);
   height: 25px;
   width: 150px;
@@ -76,21 +76,17 @@ button{
   text-align: center;
   display: block;
 
-
-  &:hover{
-    background-color:white;
-    color:  rgb(52, 53, 53);
+  &:hover {
+    background-color: white;
+    color: rgb(52, 53, 53);
   }
-  &:active{
+  &:active {
     background-color: darkgray;
     color: white;
   }
 }
-h3{
+h3 {
   text-align: center;
   margin: 45px 0px 30px 0px;
-
 }
-
 </style>
-
